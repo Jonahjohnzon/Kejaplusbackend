92a4c48ae837b4d0f88a017dccd6b82ca24157d7
Ominyi Jonah <jonahjohnzon@gmail.com>
2024-10-23 10:34:20 +0100

    update

---
+++ Controller/ValidationSchema/Schema.js
@@ -0,0 +1,18 @@
+const yup = require('yup')
+
+
+//signUp Validation
+const registrationSchema = yup.object().shape({
+    name: yup.string().required('Username is required'),
+    email: yup.string().email('Invalid email format').required('Email is required'),
+    role:yup.string().oneOf(['tenant', 'admin', 'none','manager','superadmin'], 'Invalid role'),
+  })
+
+//login Validation
+const loginSchema = yup.object().shape({
+  email: yup.string().email('Invalid email format').required('Email is required'),
+  password: yup.string().required('Password is required'),
+})
+
+module.exports.registrationSchema  = registrationSchema 
+module.exports.loginSchema = loginSchema
